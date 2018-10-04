fe5ac6504fefbad877c97a7c4a428346f253f5aa
Andrew Burke <andrew@simulatedsentience.com>
2018-10-04 08:20:20 -0400

    Added SleepThread

---
+++ SleepThread.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class SleepThread implements Runnable
+{
+	int sleepTime = 0;
+	ArrayList<Integer> theList;
+	
+	public SleepThread(int n, ArrayList<Integer> list)
+	{
+		sleepTime = n * 100;
+		list = theList;
+	}	
+	
+	@Override public void run()
+	{
+		//Sleep for a specific amount of time
+		try {
+			Thread.sleep(sleepTime);
+		}
+		catch(Exception e) {}
+		
+		//Add a value to the list
+		theList.add(sleepTime / 100);
+	}
+
+}
