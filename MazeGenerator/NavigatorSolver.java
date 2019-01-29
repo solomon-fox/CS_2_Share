048ae1280294880c5eab4a4e7c4a065452b90e8e
Andrew Burke <andrew@simulatedsentience.com>
2019-01-29 11:30:12 -0500

    Cleared for Spring 2019

--- MazeGenerator/NavigatorSolver.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Point;
-import java.util.ArrayList;
-
-
-public interface NavigatorSolver
-{
-	public ArrayList<Point> generateSolution(char[][] maze, Point start, Point end);
-	public String getMessage();
-}
