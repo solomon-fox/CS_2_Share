013b1e257e8f3f7c2a79cdf95f78513bcee1e401
Andrew Burke <andrew@simulatedsentience.com>
2019-03-11 18:10:16 -0400

    Added complete LinkedListManager

--- DataStructures/LinkedListManager.java
+++ DataStructures/LinkedListManager.java
@@ -1,12 +1,13 @@
+import java.awt.Point;
 
 public class LinkedListManager
 {
-	LinkedListNode root = null;
+	LinkedListPointNode root = null;
 	
 	public LinkedListManager()
 	{	}
 	
-	public void add(LinkedListNode newNode)
+	public void add(LinkedListPointNode newNode)
 	{
 		if(root == null)
 			root = newNode;
@@ -16,7 +17,7 @@ public class LinkedListManager
 			//so, let's follow the list until we find a node with no next
 			
 			//use the "pointer" to next nodes to jump through the list
-			LinkedListNode runner = root;	//not a copy, but a pointer
+			LinkedListPointNode runner = root;	//not a copy, but a pointer
 			
 			while(runner.next != null)
 				runner = runner.next;
@@ -27,7 +28,73 @@ public class LinkedListManager
 		}
 	}
 	
+	public int size()
+	{
+		if(root == null)
+			return 0;
+		
+		int count = 1;
+		LinkedListPointNode runner = root;
+		while(runner.next != null)
+		{
+			runner = runner.next;
+			count++;
+		}
+		return count;
+	}
 	
+	public LinkedListPointNode get(int index)
+	{
+		if(root == null)
+			return null;
+		
+		int count = 0;
+		LinkedListPointNode runner = root;
+		while(runner.next != null && count != index)
+		{
+			runner = runner.next;
+			count++;
+		}
+		return runner;
+	}
+	
+	public LinkedListPointNode remove(int index)
+	{
+		if(root == null)
+			return null;
+		
+		if(index == 0)
+			root = root.next;
+		
+		int count = 1;
+		LinkedListPointNode runner = root.next, lastNode = root;
+		while(count != index)
+		{
+			lastNode = runner;
+			runner = runner.next;
+			count++;
+		}
+		
+		lastNode.next = runner.next;
+		return runner;
+	}
+	
+	public int find(Point p)
+	{
+		if(root != null)
+		{
+			LinkedListPointNode runner = root;
+			int count = 0;
+			while(runner != null)
+			{
+				if(runner.value.equals(p))
+					return count;
+				runner = runner.next;
+				count++;
+			}
+		}
+		return -1;
+	}
 	
 }
 
