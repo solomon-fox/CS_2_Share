54e1bcfa76aef3f64c2925fa0c76271faf90fc32
Andrew Burke <andrew@simulatedsentience.com>
2019-03-14 10:37:33 -0400

    Added BSTTester for Binary Search Tree intro

---
+++ Trees/BSTTester.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BSTTester
+{
+	BinarySearchTree tree = new BinarySearchTree();
+	
+	public BSTTester()
+	{
+		Scanner in = new Scanner(System.in);
+		
+		String s = "";
+		
+		while(true)
+		{
+			System.out.print("Enter token, or -1 to stop: ");
+			s = in.nextLine();
+			if(s.equals("-1"))
+				break;
+			tree.insert( new BSTNode(s) );	//tree.RecursiveInsert(new BSTNode(s));
+		}
+		in.close();
+		
+		System.out.println("Here's the complete tree, in order: ");
+		tree.traverse();
+	}
+	
+	public static void main(String[] args)
+	{
+		new BSTTester();
+	}
+}
